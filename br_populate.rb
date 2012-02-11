e855bf756bdf41dba03b1cf7e356a56a1c40fdfd
Celso Dantas <celsodantas@gmail.com>
2012-02-11 10:30:39 -0200

    making the code little sorter

--- br_populate.rb
+++ br_populate.rb
@@ -1,19 +1,25 @@
 require 'net/http'
 require 'json'
 
-path = '/celsodantas/city_state_populate--BR/master/states.json'
-http = Net::HTTP.new('raw.github.com', 443)
-http.use_ssl = true
-states = JSON.parse http.get(path).body
+module BRPopulate
+  def self.states
+    http = Net::HTTP.new('raw.github.com', 443); http.use_ssl = true
+    JSON.parse http.get('/celsodantas/city_state_populate--BR/master/states.json').body
+  end
 
-def capital?(city, state)
-  city["name"] == state["capital"]
-end
+  def self.capital?(city, state)
+    city["name"] == state["capital"]
+  end
 
-states.each do |state|
-  state_obj = State.create(:acronym => state["acronym"], :name => state["name"])
+  def self.populate
+    states.each do |state|
+      state_obj = State.create(:acronym => state["acronym"], :name => state["name"])
 
-  state["cities"].each do |city|
-    City.create(:name => city, :state => state_obj, :capital => capital?(city, state))
+      state["cities"].each do |city|
+        City.create(:name => city, :state => state_obj, :capital => capital?(city, state))
+      end
+    end
   end
 end
+
+BRPopulate.populate
