1ca18d4ca1803dc8c73f2b8d1efc9f304804ed35
Jan Silva <janfrs3@gmail.com>
2016-06-06 23:59:56 -0300

    Populate regions

--- br_populate.rb
+++ br_populate.rb
@@ -14,9 +14,10 @@ module BRPopulate
 
   def self.populate
     states.each do |state|
-      state_obj = State.new(:acronym => state["acronym"], :name => state["name"])
+      region_obj = Region.find_or_create_by(:name => state["region"])
+      state_obj = State.new(:acronym => state["acronym"], :name => state["name"], :region => region_obj)
       state_obj.save
-      
+
       state["cities"].each do |city|
         c = City.new
         c.name = city
